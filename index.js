fbe95f5fe36ba33cd82818577c86646fbbe620e0
Brandon <brandon139@protonmail.com>
2023-12-28 13:58:28 +1100

    first

---
+++ index.js
@@ -0,0 +1,23 @@
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./sample.db', (err) => {
+if (err) {
+  return console.error(err.message);
+}
+console.log('Connected to the in-memory SQlite database.');
+});
+
+db.serialize(() => {
+    db.run("CREATE TABLE lorem (info TEXT)");
+
+    const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+    for (let i = 0; i < 10; i++) {
+        stmt.run("Ipsum " + i);
+    }
+    stmt.finalize();
+
+    db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
+        console.log(row.id + ": " + row.info);
+    });
+});
+
+db.close();
